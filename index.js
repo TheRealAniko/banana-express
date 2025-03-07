19475861a8286c3239125ba0de359237becf5570
Razvan Andrei <chiper_raizy@yahoo.com>
2025-03-07 17:33:53 +0100

    adding database connection with postgres

---
+++ index.js
@@ -0,0 +1,28 @@
+import express from "express"; // import express
+import cors from "cors"; // import cors
+import dotenv from "dotenv"; // import dotenv
+import { connectDB } from "./db/index.js"; // import connectDB function
+
+dotenv.config(); // load environment variables
+
+const app = express(); // create an express app
+app.use(cors()); // middleware cors  enable CORS for all requests
+app.use(express.json()); // middleware for JSON parsing
+
+const PORT = process.env.PORT || 5000; // set the port
+
+// create a route
+app.get("/", (req, res) => {
+  res.send("hello world from nodejs!!!!!");
+}); //msg from nodejs
+
+// connect to db and start server
+const startServer = async () => {
+  await connectDB(); // connect to the database
+  app.listen(PORT, () =>
+    console.log(`server running on port ${PORT} ->  http://localhost:${PORT}/`)
+  ); // server port 5000
+};
+
+// start the server
+startServer();
